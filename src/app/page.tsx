09c6297223e871c057e1d39daca3c2a9fdeb168b
ImTonyS <tony.villaverde@hotmail.com>
2025-05-26 11:38:38 -0600

    Error fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import { DATA } from "@/data/resume";
 import Link from "next/link";
+import { useState } from "react";
 import Markdown from "react-markdown";
 
 const BLUR_FADE_DELAY = 0.04;
@@ -144,7 +145,6 @@ export default function Page() {
                   dates={project.dates}
                   tags={project.technologies}
                   image={project.image}
-                  video={project.video}
                   links={project.links}
                 />
               </BlurFade>
